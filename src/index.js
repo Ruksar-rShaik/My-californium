7048ff8928d3fcfddd30dd02203586a0ae0da3e7
ruksar <ruksarrasheeda786@gmail.com>
2022-12-23 15:43:46 +0530

    firdt commit

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const { default: mongoose } = require('mongoose');
+mongoose.set('strictQuery', true)
 const app = express();
-
+const commonMW = require('./middlewares/commonMiddlewares')
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -14,12 +15,7 @@ mongoose.connect("mongodb+srv://functionup-cohort:G0Loxqc9wFEGyEeJ@cluster0.rzot
 .then( () => console.log("MongoDb is connected"))
 .catch ( err => console.log(err) )
 
-app.use (
-    function (req, res, next) {
-        console.log ("inside GLOBAL MW");
-        next();
-  }
-  );
+app.use (commonMW.mid1);
 
 app.use('/', route);
 
--- src/routes/route.js
+++ src/routes/route.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 // const UserModel= require("../models/userModel.js")
 const UserController= require("../controllers/userController")
-const BookController= require("../controllers/bookController")
+//const BookController= require("../controllers/bookController")
 const commonMW = require ("../middlewares/commonMiddlewares")
+
+
 
 router.get("/test-me", function (req, res) {
     res.send("My first ever api!")
@@ -11,8 +13,21 @@ router.get("/test-me", function (req, res) {
 
 
 
+router.get("/basicRoute",commonMW.mid1,UserController.printDate )
 
-router.post("/createBook", BookController.createBook  )
+
+
+
+
+
+
+
+
+
+
+
+
+//router.post("/createBook", BookController.createBook  )
 
 
 
@@ -48,7 +63,7 @@ router.post("/createBook", BookController.createBook  )
 
 
 
-router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
+//router.get("/basicRoute", commonMW.mid1, commonMW.mid2, commonMW.mid3, commonMW.mid4, UserController.basicCode)
 
 
 
